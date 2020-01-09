58c33b6e455823fd274f9ae5cbc31c6fe067529f
ashish1697 <38009643+ashish1697@users.noreply.github.com>
2020-01-09 23:02:30 +0530

    commit

---
+++ jasmine-standalone-2.3.0/spec/CalSpec.js
@@ -0,0 +1,11 @@
+describe("Hello World", function(){
+it("Says Hello",function(){
+	
+	expect(HelloWorld()).toEqual("HelloWorld");
+});
+it("Says boy",function(){
+	
+	expect(HelloWorld()).toEqual("HelloWorld1");
+});
+
+});
